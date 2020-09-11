a1f9cbd9c2982c7bb80c2c6549ee171dd31bddf2
Paul Brian Marcuelo <paul.brian@kodakollectiv.com>
2020-09-11 12:38:12 +0800

    Image Upload Validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
     #Validate Form
     validates :title, presence: true
     validates :body, presence: true
+    validate :image_type
 
     #Article Relationship to Active Storage for Image
     has_many_attached :images, dependent: :destroy
@@ -10,5 +11,15 @@ class Article < ApplicationRecord
     #Image Sizes
     def thumbnail input
         return self.images[input].variant(resize: "300x300!").processed        
+    end
+
+    private
+    #Image Validation
+    def image_type
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png'))
+                errors.add(:images, "needs to be JPEG or PNG")
+            end
+        end
     end
 end
