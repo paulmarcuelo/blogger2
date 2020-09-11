40bc241774c9a07401e771404cd2861659812764
Paul Brian Marcuelo <paul.brian@kodakollectiv.com>
2020-09-11 11:26:32 +0800

    RESTful APIs WITH image upload

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ApplicationRecord
+
+    #Validate Form
     validates :title, presence: true
     validates :body, presence: true
+
+    #Article Relationship to Active Storage for Image
+    has_many_attached :images, dependent: :destroy
+
+    #Image Sizes
+    def thumbnail input
+        return self.images[input].variant(resize: "300x300!").processed        
+    end
 end
