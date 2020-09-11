f20e118ccd33c553ecdfb9b710a9d4050113d7e3
Paul Brian Marcuelo <paul.brian@kodakollectiv.com>
2020-09-11 11:03:41 +0800

    RESTful APIs without image upload

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+    include ArticlesHelper
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            flash.notice = "Article '#{@article.title}' Updated!"
+            redirect_to article_path(@article)
+        else
+            flash.notice = "Rats! Please fix your mistakes!"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        redirect_to articles_path
+    end
+end
